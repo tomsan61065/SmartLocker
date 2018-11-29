b287e8d0be7ddf9f5cdc1f0f0ccc24ca22b14dfb
tomsan61065 <tomsangotw@gmail.com>
2018-11-29 23:55:37 +0800

    version 2

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -4,8 +4,12 @@
 const express = require('express'); 
 const keythereum = require('keythereum');
 const userKeyfile = require('../../keyfile/mykey.json');
+const util = require("ethereumjs-util");
 
 const router = express.Router();
+const AES = require("crypto-js").AES;
+const ecies = require("eth-ecies");
+const request = require("request");
 
 var {
   web3,
@@ -29,7 +33,9 @@ var lockerContract = new web3.eth.Contract(
   LockerAbi,
 //  { gas: 4000000, gasPrice: '500000000000' }, //不給會怎樣?? 沒差會自己抓
   //要自己給 contract address
-  "0xAd728b56377E54869E8131406745986d16ed2655",
+  //"0xAd728b56377E54869E8131406745986d16ed2655", // <============= token address 要自己給
+  //"0x856B8Ff4A21E0992fCbff2aBae9F2E38A067b9d0", //新版 address
+  "0xF0B061d189da1E692d13bB3E802d628920D1E255", //mapping 改 address array
 );
 
 router.get('/', (req, res) => {
@@ -40,13 +46,13 @@ router.get("/create" , async(req, res) => {
 
   let myContract = new web3.eth.Contract(
     LockerAbi,
-    { gas: 4000000, gasPrice: 0 },
+    { gas: 4700000, gasPrice: 0 },
   );
 
   let data = await myContract
   .deploy({
     data: LockerBin,
-    arguments: ["0x3e0315B3dAA3C888EfA5a0F5AC75db97f1c01437"], //ncco token address required
+    arguments: ["0x3e0315B3dAA3C888EfA5a0F5AC75db97f1c01437"], //nccu token address required
   }).encodeABI();
 
   let privateKey = await keythereum.recover('techfin', userKeyfile);
@@ -55,7 +61,7 @@ router.get("/create" , async(req, res) => {
   let transaction = {
     data,
     nonce,
-    gas: 4000000,
+    gas: 0x47b760,
     gasPrice: 0,
     value: '0x0',
   };
@@ -63,13 +69,15 @@ router.get("/create" , async(req, res) => {
   let rawTx = signTX(privateKey, transaction);
 
   web3.eth.sendSignedTransaction(rawTx).on("receipt", (receipt) => {
-    lockerContract.options.address = receipt.contractAddress;
+  //  lockerContract.options.address = receipt.contractAddress;
     console.log(receipt.contractAddress);
     
     res.json({
-      address : lockerContract.options.address, //如果不是在promise裡面，而是在外邊會變成 "address":null
+      test: "test",
+      //address : lockerContract.options.address, //如果不是在promise裡面，而是在外邊會變成 "address":null
     });
-  });
+  })
+  .on("error", console.log);
 
 });
 
@@ -130,26 +138,40 @@ router.get("/returnLock", async (req, res) => {
   res.send("return lock");
 });
 
+
 router.get("/openLock", async (req, res) => {
+  var symkey = "";
 
-  let privateKey = await keythereum.recover('techfin', userKeyfile);
-  let nonce = await web3.eth.getTransactionCount('0x' + userKeyfile.address);
-  let data = await lockerContract.methods.openLock().encodeABI();
+  let privateKey = await keythereum.recover('techfin', userKeyfile); //這要花5秒生成，太慢
+  let publicKey = util.privateToPublic(privateKey).toString("hex"); 
+//  console.log(publicKey);
+  request('http://localhost:3000/verify_1/' + publicKey, async function (error, response, body) { 
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //  console.log('body:', body); // Print the HTML for the Google homepage.
+    symkey = await decrypt(privateKey, body);
+    let returnData = await AES.encrypt("0x" + userKeyfile.address, symkey).toString();
+  //  console.log(returnData);
+    
+    request('http://localhost:3000/verify_2/' + returnData, function (error, response, body){
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML for the Google homepage.
+    });
 
-  let transaction = {
-    data,
-    nonce,
-    gas: 2000000,
-    gasPrice: 0,
-    to: lockerContract.options.address,
-    value: "0x0",
-  };
-
-  let rawTx = signTX(privateKey, transaction);
-  web3.eth.sendSignedTransaction(rawTx).on('receipt', console.log);
+  });
 
   res.send("open lock");
 });
+
+function decrypt(privateKey, encryptedData) {
+  let userPrivateKey = new Buffer(privateKey, 'hex');
+  let bufferEncryptedData = new Buffer(encryptedData, 'base64');
+
+  let decryptedData = ecies.decrypt(userPrivateKey, bufferEncryptedData);
+  
+  return decryptedData.toString('utf8');
+}
 
 // @uint256 _value
 router.post("/changePrice", async (req, res) => {
@@ -241,5 +263,57 @@ router.post("/getTokenBack", async (req, res) => {
     to: _to,
   });
 });
+
+
+//@address _others
+router.post("/addWhiteList", async (req, res) => {
+  let privateKey = await keythereum.recover("techfin", userKeyfile);
+  let nonce = await web3.eth.getTransactionCount("0x" + userKeyfile.address);
+  let _others = req.body.others;
+  let data = await lockerContract.methods.addWhiteList(_others).encodeABI();
+
+  let transaction = {
+    data,
+    nonce,
+    gas: 2000000,
+    gasPrice: 0,
+    to: lockerContract.options.address,
+    value: "0x0",
+  };
+
+  let rawTx = signTX(privateKey, transaction);
+  web3.eth.sendSignedTransaction(rawTx).on("receipt", console.log);
+});
+
+//@address _others
+router.post("/removeWhiteList", async (req, res) => {
+  let privateKey = await keythereum.recover("techfin", userKeyfile);
+  let nonce = await web3.eth.getTransactionCount("0x" + userKeyfile.address);
+  let _others = req.body.others;
+  let data = await lockerContract.methods.removeWhiteList(_others).encodeABI();
+
+  let transaction = {
+    data,
+    nonce,
+    gas: 2000000,
+    gasPrice: 0,
+    to: lockerContract.options.address,
+    value: "0x0",
+  };
+
+  let rawTx = signTX(privateKey, transaction);
+  web3.eth.sendSignedTransaction(rawTx).on("receipt", console.log);
+});
+
+//有無被租借
+router.get("/isOwned", async(req, res) => {
+
+  let address = await lockerContract.methods.owner().call();
+
+  res.json({
+    address: address,
+  });
+});
+
 
 module.exports = router;
--- src/contract/Locker.sol
+++ src/contract/Locker.sol
@@ -1,68 +1,110 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.23;
 
-import "./interface/ERC20.sol";
+import "./Exchange.sol";
 
-contract Locker {
-    address public admin;
+contract locker{
+
+    address private master;
     address public owner;
-    
+
     address public tokenAddress;
-    
-    string public name;
-    uint256 public decimal = 18;
+    uint public decimal = 18;
 
-    // 租賃價格 先給予一個固定的值
-    uint256 public rentPrice = 10 * (10 ** decimal); 
+    uint public rentPrice = 10 * (10 ** decimal);
 
-    event LockOpen(address indexed who, uint256 blockTime);
+    //事件
+    event rentLockEvent(address indexed from, uint timestamp); 
+    event returnLockEvent(address indexed from, uint timestamp);
+    //event openLockEvent(address indexed from, uint timestamp);
+    event changePriceEvent(address indexed from, uint timestamp);
+    event changeTokenAddressEvent(address indexed from, uint timestamp);
+    event setDecimalEvent(address indexed from, uint value, uint timestamp);
+    event getTokenBackEvent(address indexed from, address indexed to, uint value, uint timestamp);
 
-    // 管理者創建立智能合約
-    // _name => 名稱
-    // _tokenAddress => erc20 之 address
-    function Locker(string _name, address _tokenAddress) public {
-        name = _name;
-        admin = msg.sender;
+    address[] whiteList;
+
+    //初始化
+    constructor(address _tokenAddress) public {
+        master = msg.sender;
         tokenAddress = _tokenAddress;
     }
 
-    // 租吝 遇租吝之人 要自己call 這個function
-    // 並在運作此func之前要先 call 原本的Token Contract 之 approve 
-    function rent() public returns (bool) {
-        // remember to approved befor transfer From
-        require(ERC20(tokenAddress).transferFrom(msg.sender, this, rentPrice));
+    //收 erc 20 跟動鎖的 owner
+    //https://www.reddit.com/r/ethdev/comments/73mzjr/how_do_you_interact_with_any_erc20_in_a_smart/
+    //應付 contract call contract 的問題
+    //在運作此func之前要先 call 原本的Token Contract 之 approve 
+    function rentLock() public {
+        //先檢查有無租借出去
+        require(owner == address(0)); // address 是否為 0x0
+        //所以用 transferFrom 而非 transfer
+        require(ERC223Interface(tokenAddress).transferFrom(msg.sender, this, rentPrice));
         owner = msg.sender;
-        return true;
+        addWhiteList(owner);
+        
+        emit rentLockEvent(msg.sender, now); //emit call event用
     }
 
-    // 退租 可以由使用者或是管理者 進行
-    function checkout() public returns (bool) {
-        require(msg.sender == owner || msg.sender == admin);
-        owner = address(0);
-        return true;
+    //轉回給 master
+    function returnLock() public {
+        require(owner == msg.sender || master == msg.sender, "Owner required.");
+        owner = address(0); //丟垃圾, 銷毀
+        for(uint i = 0; i < whiteList.length; i++){
+            delete whiteList[i];
+        }
+
+        emit returnLockEvent(msg.sender, now);
     }
 
-    function openLock() public returns (bool) {
-        require(msg.sender == owner || msg.sender == admin);
-        emit LockOpen(msg.sender, block.timestamp);
-        return true;
+    //新增至白名單
+    function addWhiteList(address _others) public {
+        require(msg.sender == owner);
+        
+        whiteList.push(_others);
     }
-    
-    // 管理員可以更改價格 
-    // 請注意此處的價格是 有乘上decimaled 的 default 18
-    function setRentPrice(uint256 _rentPrice) public returns (bool) {
-        require(msg.sender == admin);
-        rentPrice = _rentPrice;
-        return true;
+    //從白名單移除
+    function removeWhiteList(address _others) public {
+        require(msg.sender == owner);
+        for(uint i = 0; i < whiteList.length; i++){
+            if(_others == whiteList[i]){
+                delete whiteList[i];
+            }
+        }
     }
-    
-    // 管裡員可以更改對應的 token address
-    function setTokenAddress(address _tokenAddress) public {
-        require(msg.sender == admin);
-        tokenAddress = _tokenAddress;
+    //開鎖
+    function openLock(address _visitor) external view returns (uint256) {
+        //回傳是否在白名單內，locker那邊發現是就開門
+        
+        if(_visitor == master){ // 管理員權限
+            return 1;
+        }
+        for(uint i = 0; i < whiteList.length; i++){
+            if(_visitor == whiteList[i]){
+                return 1;
+            }
+        }
+        return 0;
     }
-    // 管理員可以更改計價token 之 decimal
-     function setDecimal(uint _decimal) public {
+
+    //管理員更動價格
+    function changePrice(uint _value) public {
         require(msg.sender == master, "Master required.");
+        rentPrice = _value;
+
+        emit changePriceEvent(msg.sender, now);
+    }
+
+    //更改 token address
+    function changeTokenAddress(address _tokenAddress) public {
+        require(msg.sender == master, "Master required.");
+        tokenAddress = _tokenAddress;
+
+        emit changeTokenAddressEvent(msg.sender, now);
+    }
+
+    // 管理員可以更改計價token 之 decimal
+    function setDecimal(uint _decimal) public {
+        require(msg.sender == master, "Master required.");
+        require(_decimal != 0, "decimal can not be zero.");
         rentPrice = rentPrice / decimal;
         decimal = _decimal;
         rentPrice = rentPrice * (10 ** decimal);
@@ -71,14 +113,13 @@ contract Locker {
     }
 
     // 管理員可以將此contract 擁有的 token 轉給某個人
+    // 回收 token
     function getTokenBack(address _to) public returns (bool) {
-        require(msg.sender == admin);
-        uint256 tokenLeft = ERC20(tokenAddress).balanceOf(this);
-        return ERC20(tokenAddress).transfer(_to, tokenLeft);
+        require(msg.sender == master, "Master required.");
+        uint tokenLeft = ERC223Interface(tokenAddress).balanceOf(this);
+        return ERC223Interface(tokenAddress).transfer(_to, tokenLeft); //聖文: 這樣寫就對了
+
+        emit getTokenBackEvent(msg.sender, _to, tokenLeft, now);
     }
 
-    // 拒絕接收 eth 幣
-    function () public payable {
-        revert();
-    }
 }
